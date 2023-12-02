0d4eae01ede6d151b2614698af1c8541d19a3b29
Karuch <talk159820@gmail.com>
2023-12-02 22:18:38 +0200

    should be able to handle the API now by adding fetches requests to api_handler.js

--- api_handler.js
+++ api_handler.js
@@ -1,28 +1,40 @@
-const now = new Date();
-const year = now.getFullYear();        // Returns the year (4 digits)
-const month = now.getMonth() + 1;      // Returns the month (0-11, +1 to make it 1-12)
-const day = now.getDate();             // Returns the day of the month (1-31)
-const hours = now.getHours();          // Returns the hour (0-23)
-const minutes = now.getMinutes();      // Returns the minutes (0-59)
-const seconds = now.getSeconds();      // Returns the seconds (0-59)
-const milliseconds = now.getMilliseconds(); // Returns the milliseconds (0-999)
-const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-const dateElement = document.getElementById('date');
-// Set the text of the h1 element to the title from the JSON
-dateElement.textContent = formattedDateTime
-
 fetch('https://dummyjson.com/products/1')
   .then(res => res.json())
   .then(json => {
-    // Select the h1 element by its id
-    const titleElement = document.getElementById('expandingTextArea5');
+    // Select the textarea element by its id
+    const textareaElement = document.getElementById('expandingTextArea5');
 
-    // Set the text of the h1 element to the title from the JSON
-    titleElement.textContent = json.title;
+    // Append the new text to the textarea, starting two lines down if there's already text
+    if (textareaElement.value) {
+      textareaElement.value += '\n\n';
+    }
+    textareaElement.value += json.title;
+
+    // Adjust the height of the textarea
+    textareaElement.style.height = 'auto'; // Reset the height
+    textareaElement.style.height = textareaElement.scrollHeight + 'px'; // Set the height to the content's scroll height
   })
   .catch(error => {
     console.error('Error:', error);
   });
-  
-  
+
+
+  fetch('https://dummyjson.com/products/1')
+  .then(res => res.json())
+  .then(json => {
+    // Select the textarea element by its id
+    const textareaElement = document.getElementById('expandingTextArea5');
+
+    // Append the new text to the textarea, starting two lines down if there's already text
+    if (textareaElement.value) {
+      textareaElement.value += '\n\n';
+    }
+    textareaElement.value += json.price;
+
+    // Adjust the height of the textarea
+    textareaElement.style.height = 'auto'; // Reset the height
+    textareaElement.style.height = textareaElement.scrollHeight + 'px'; // Set the height to the content's scroll height
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
+const now = new Date();
+const year = now.getFullYear();        // Returns the year (4 digits)
+const month = now.getMonth() + 1;      // Returns the month (0-11, +1 to make it 1-12)
+const day = now.getDate();             // Returns the day of the month (1-31)
+const hours = now.getHours();          // Returns the hour (0-23)
+const minutes = now.getMinutes();      // Returns the minutes (0-59)
+const seconds = now.getSeconds();      // Returns the seconds (0-59)
+const milliseconds = now.getMilliseconds(); // Returns the milliseconds (0-999)
+const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+const dateElement = document.getElementById('date');
+// Set the text of the h1 element to the title from the JSON
+dateElement.textContent = formattedDateTime
+
+
+
+
 function expandings_handler(expanding_id){
 
     const textarea = document.getElementById(expanding_id);
